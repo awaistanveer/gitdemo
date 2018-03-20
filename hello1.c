70e1bff8bc988de3e2dd04d7d1a166d5d08cf3ab
Awais Tanveer <atanveer@aut.ac.nz>
2018-03-20 16:37:49 +1300

    This is a new commit in a new branch

---
+++ hello1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void commit3() {
+	printf("This is commit 2");
+}
+
+void br1() {
+	printf("A function in new branch\n");	
+}
+int main() {
+	printf("A New branch.\n");
+	return 0;
+}
