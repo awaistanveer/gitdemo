9c1479c20b3f40fa5561a441f840af09465f3bfc
Awais Tanveer <atanveer@aut.ac.nz>
2018-03-20 15:51:31 +1300

    this is the second commit

--- hello.c
+++ hello.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-void commit1()
-{
+void commit2() {
+	printf("This is commit 2");
+}
+
+void commit1() {
 	return;	
 }
 
 void br1() {
 	printf("A function in new branch\n");	
 }
-int main()
-{
+int main() {
 	br1();
 	printf("A branch merged.\n");
 	return 0;
